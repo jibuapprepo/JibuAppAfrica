5dfe5c1c351f22e839bc8375c9c5b60bd6ded408
jibuapprepo <jibu@napvibe.com>
2025-08-15 20:24:27 +0300

    Delete src/core/features/jibu directory

--- src/core/features/jibu/jibu.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { JibuPage } from './jibu';
-
-@NgModule({
-  declarations: [JibuPage],
-  imports: [IonicPageModule.forChild(JibuPage)]
-})
-export class JibuPageModule {}
