11ac8c1354c9336f346a25874356e09b00e08c52
jibuapprepo <jibu@napvibe.com>
2025-08-15 20:34:41 +0300

    Update jibu-routing-registration.module.ts

--- src/app/jibu-routing-registration.module.ts
+++ src/app/jibu-routing-registration.module.ts
@@ -1,1 +1,18 @@
 
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// This route definition will be merged into the main routing when imported in app.module.ts
+const routes: Routes = [
+  {
+    path: 'jibu',
+    loadChildren: () =>
+      import('src/core/features/jibu/jibu.module').then(m => m.JibuPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class JibuRoutingRegistrationModule {}
